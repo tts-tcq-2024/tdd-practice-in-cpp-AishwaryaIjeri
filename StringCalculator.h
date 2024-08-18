7df9d8144881573fd6d4f6eb0568c8faca0ee78f
AishwaryaIjeri <aishuijeri@gmail.com>
2024-08-18 22:33:15 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,18 @@
+cpp
+Copy code
+#ifndef STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
 
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+class StringCalculator {
+public:
+    static int Add(const std::string& numbers);
+private:
+    static std::vector<int> parseNumbers(const std::string& input);
+    static void validateNumbers(const std::vector<int>& numbers);
+};
+
+#endif // STRING_CALCULATOR_H
