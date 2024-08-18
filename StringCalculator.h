17f4c17fa427be8bc80b67684b1766a8518202ee
AishwaryaIjeri <aishuijeri@gmail.com>
2024-08-18 22:36:02 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,5 +1,3 @@
-cpp
-Copy code
 #ifndef STRING_CALCULATOR_H
 #define STRING_CALCULATOR_H
 
@@ -10,9 +8,12 @@ Copy code
 class StringCalculator {
 public:
     static int Add(const std::string& numbers);
+
 private:
     static std::vector<int> parseNumbers(const std::string& input);
     static void validateNumbers(const std::vector<int>& numbers);
+    static std::string extractDelimiter(const std::string& input);
+    static std::string removeDelimiterLine(const std::string& input);
 };
 
 #endif // STRING_CALCULATOR_H
